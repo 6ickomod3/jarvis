01ce322b277f604fc4e1d0d4570a88eb6dec9d8e
6ickomod3 <jidai2014@gmail.com>
2025-12-09 16:41:09 -0800

    Initial Commit

---
+++ LifeLogger/LifeLoggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeLoggerApp.swift
+//  LifeLogger
+//
+//  Created by Ji Dai on 12/9/25.
+//
+
+import SwiftUI
+
+@main
+struct LifeLoggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LifeLoggerTests/LifeLoggerTests.swift
@@ -0,0 +1,17 @@
+//
+//  LifeLoggerTests.swift
+//  LifeLoggerTests
+//
+//  Created by Ji Dai on 12/9/25.
+//
+
+import Testing
+@testable import LifeLogger
+
+struct LifeLoggerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
